e4eab9a17d45205ff91572e62ef2e2ba9609c0df
shenxf1986 <185693240@qq.com>
2019-12-31 13:17:02 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+import time;
+
+# 定义一些常量
+host = 'https://www.dytt8.net' # 电影天堂根地址
+url = '/index0.html' # 电影天堂主页
+
+# headers内容设定
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36'
+}
+
+# 获取页面
+def get_page(link):
+    # 请求这个网站，并得到网站的内容
+    r = requests.get(link, headers= headers)
+    html = r.content # 使用r.content解封装
+    html = html.decode('gbk') # 由gb2312解码为unicode
+    soup = BeautifulSoup(html, 'lxml')
+    return soup
+
+# 解析网页
+def get_data(filmName_list):
+    for each in filmName_list:
+        # 去除广告等其他内容
+        if each['href'] == '/html/gndy/dyzz/index.html' or each['href'] == '/app.html':
+            continue
+
+        # 获取每个电影的详细内容
+        print(each.text.strip())
+        soup = get_page(host + each['href'])
+        print (soup)
+        content = soup.find('div', id_="Zoom")
+        print (content)
+        # 以免被反爬虫，每爬一个网页就休息会
+        time.sleep(1)
+
+
+# 主进程逻辑
+soup = get_page(host + url)
+new_films=soup.find('div', class_="co_content8")
+print (new_films)
+filmName_list =new_films.find_all('a');
+get_data(filmName_list)
